2798936ffa904cf981a6ea80515562e8eaaba7fa
Wootifiable <96454459+Wootifiable@users.noreply.github.com>
2021-12-21 22:49:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var goku = {
   name: "goku",
-  gender: "male"
+  gender: "male",
+  type: "person"
 }
 var myMom = {
   name: "mother",
-  gender: "female"
+  gender: "female",
+  type: "person"
+}
+var british = {
+  name: "mother",
+  gender: "none",
+  type: "government"
 }
 
 goku = null
 myMom = null
+british = null
